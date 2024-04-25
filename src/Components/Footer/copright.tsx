2fe8ff1690b666dc5c4f8c74b2a64fd42503888f
elsnoty <mostafashawky399@gmail.com>
2024-04-25 09:18:02 +0200

    hot fix

--- src/Components/Footer/copright.tsx
+++ src/Components/Footer/copright.tsx
@@ -4,7 +4,7 @@ const CopyRight = () => {
         <div className="flex gap-5 pt-20 items-center">
             <p>Privacy policy</p>
             <div className="min-w-[2px] h-7 bg-white"></div>
-            <p>Copyright @ Kartik Bansal 2022. All Rights Reserved.</p>
+            <p>Copyright @ Kartik Bansal 2022. All Rights Reserved. </p>
             <div className="min-w-[2px] h-7 bg-white"></div>
             <p>Terms of service</p>
         </div>
